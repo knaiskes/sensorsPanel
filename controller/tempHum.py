9d3a0d50a0bf6a4abebbc9c8e7a9f5c7137b25b1
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2017-11-05 12:01:23 +0000

    added mqtt support - sends measuremts to rpi

--- controller/tempHum.py
+++ controller/tempHum.py
@@ -1,16 +1,17 @@
-#from umqtt.simple import MQTTClient
+from umqtt.simple import MQTTClient
 from machine import Pin
 import micropython
 from time import sleep
 from dht import DHT22
 
 # MQTT
-#SERVER = "192.168.2.3" # Raspberry Pi
-#CLIENT_ID = "esp_dht22_sensor"
-#TOPIC = b"tempSensor"
+SERVER = "192.168.2.3" # Raspberry Pi
+CLIENT_ID = "esp_dht22_sensor"
+TOPIC = b"tempSensor"
 
 def temp_hum():
-
+	client = MQTTClient(CLIENT_ID, SERVER)
+	client.connect()
 	sensor = DHT22(Pin(5, Pin.IN, Pin.PULL_UP))
 
 	while True:
@@ -19,10 +20,9 @@ def temp_hum():
 			temperature = sensor.temperature()
 			humidity = sensor.humidity()
 			if isinstance(temperature, float) and isinstance(humidity, float):
-				#msg = (b"{0:3.1f},{1:3.1f}".format(temperature, humidity))
 				msg = (b"{0:3.1f},{1:3.1f}".format(temperature, humidity))
 				print(msg) # to be removed later
-				#client.publish(TOPIC, msg)
+				client.publish(TOPIC, msg)
 		except OSError:
 			print("System error")
 		# sleep(3600) # one hour
