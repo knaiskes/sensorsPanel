48440e8adcaf29d6171c45bdbff8711e0c43a111
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2017-11-17 17:18:28 +0000

    added the html table

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template,redirect,request,url_for,session,g
+from databases.database import *
+#from databases.measurements import *
+import sys
+import sqlite3
+
+app = Flask(__name__)
+
+app.secret_key = "secret_key"
+
+@app.before_request
+def before_request():
+	g.db = sqlite3.connect("databases/measurements.db")
+
+@app.teardown_request
+def teardown_request(exception):
+	if hasattr(g, "db"):
+		g.db.close()
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+	error = None
+	if request.method == "POST":
+		if checkUser(request.form["username"],request.form["password"]) == False:
+			error = "Invalid login credentials"
+		else:
+			session["logged_in"] = True
+			session["username"] = request.form["username"]
+			return redirect(url_for("dashboard"))
+	return render_template("login.html",error=error)
+
+@app.route("/dashboard")
+def dashboard():
+	if not session.get("logged_in"):
+		return redirect(url_for("login"))
+	else:
+		temp = g.db.execute("SELECT temperature FROM measurements").fetchall()
+		return render_template("dashboard.html", temp = temp)
+
+"""
+@app.route("/temp")
+def temp():
+	temp = g.db.execute("SELECT temperature FROM measurements").fetchall()
+	return render_template("temp.html", temp = temp)
+"""
+
+if __name__ == "__main__":
+	app.run(debug=True, host="0.0.0.0")
